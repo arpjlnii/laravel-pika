2f510d1cf1fbeb67ab8c0166930723d63c13b5a4
arpika juliani <julianiarpika@gmail.com>
2025-02-04 13:18:55 +0700

    tugas-crud

--- resources/views/template/separate/sidebar.blade.php
+++ resources/views/template/separate/sidebar.blade.php
@@ -10,10 +10,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/farelGelap.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Farel</a>
+          <a href="#" class="d-block">Arpika</a>
         </div>
       </div>
 
